5773cfa66843ec3896f58408f18c4365b2f9501b
Solange <sol.cer.87@gmail.com>
2019-07-14 11:45:16 +0100

    Final tweaks

--- application.py
+++ application.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, redirect, url_for, request
 from datetime import datetime
 from pytz import timezone    
 
- 
 app = Flask(__name__)
 
 def get_french_time():
@@ -11,12 +10,14 @@ def get_french_time():
     #return pa_time.strftime('%Y-%m-%d_%H-%M-%S')
     return pa_time.strftime('%H : %M')
 
+
 @app.route("/", methods=["GET"])
 def home_page():
     #var1 = datetime.datetime.now()
     var1 = get_french_time()
 
     return render_template("frenchtime.html", dynamiccontent=var1)
+
 
 if __name__ == "__main__":
     app.run(debug=True)  
